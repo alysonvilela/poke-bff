89a977d6248dfc8ca08247fafe2fce74fff4555c
Alyson Vilela do Nascimento <alyson.nascimento.cit@naturapay.net>
2024-06-27 07:58:26 -0300

    setup: configure react query

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,9 +2,14 @@ import '../styles/globals.css'
 import '../config/envs'
 
 import type { AppProps } from 'next/app'
+import { ReactQueryProvider } from '../src/services/rk-config'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <ReactQueryProvider>
+     <Component {...pageProps} />
+    </ReactQueryProvider>
+  )
 }
 
 export default MyApp
